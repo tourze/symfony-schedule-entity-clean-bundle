8d8eb261c0a612580d459bd73ac3c293d75a0403
github-ci <you@example.com>
2025-06-13 14:56:49 +0800

    新增对 `tourze/bundle-dependency` 和 `tourze/symfony-cron-job-bundle` 的依赖，完善 `ScheduleEntityCleanBundle` 的依赖声明。

--- src/ScheduleEntityCleanBundle.php
+++ src/ScheduleEntityCleanBundle.php
@@ -3,7 +3,15 @@
 namespace Tourze\ScheduleEntityCleanBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Tourze\BundleDependency\BundleDependencyInterface;
+use Tourze\Symfony\CronJob\CronJobBundle;
 
-class ScheduleEntityCleanBundle extends Bundle
+class ScheduleEntityCleanBundle extends Bundle implements BundleDependencyInterface
 {
+    public static function getBundleDependencies(): array
+    {
+        return [
+            CronJobBundle::class => ['all' => true],
+        ];
+    }
 }
